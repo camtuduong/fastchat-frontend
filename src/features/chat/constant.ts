42893aa86f0bcef3b9a28badaa5aeb2216fdc49d
teq-tuduong <tuduong@teqnological.asia>
2026-06-27 15:10:11 +0700

    fix(chat): correct margin classes for bubble positioning in chat messages

--- src/features/chat/constant.ts
+++ src/features/chat/constant.ts
@@ -3,7 +3,7 @@ import type { BubblePosition } from "@/features/chat/types/bubbleChat";
 export const bubbleClass = (position: BubblePosition, isMyMessage: boolean) => {
   switch (position) {
     case "single":
-      return "mt-2 rounded-2xl";
+      return "my-2 rounded-2xl";
 
     case "first":
       return `${isMyMessage ? "rounded-l-2xl rounded-br-2xl" : "rounded-b-2xl rounded-r-2xl"}`;
@@ -12,6 +12,6 @@ export const bubbleClass = (position: BubblePosition, isMyMessage: boolean) => {
       return `${isMyMessage ? "rounded-l-2xl rounded-r-md" : "rounded-l-md rounded-r-2xl "}`;
 
     case "last":
-      return `${isMyMessage ? "rounded-l-2xl rounded-tr-2xl" : "rounded-t-2xl rounded-r-2xl"}`;
+      return `mt-2 ${isMyMessage ? "rounded-l-2xl rounded-tr-2xl" : "rounded-t-2xl rounded-r-2xl"}`;
   }
 };
